aba85ac8daa119c3b7d4e0206073a0d0d4257db7
Gregory Potemkin <gregory@gomobile.ru>
2021-10-23 00:40:23 +0300

    dataset paring made

--- backend/api/model.py
+++ backend/api/model.py
@@ -2,7 +2,8 @@ from typing import List, Optional, Tuple, Generic, TypeVar, Union
 from pydantic import BaseModel
 from pydantic.generics import GenericModel
 from datetime import datetime, date
-from .utils import *
+from pandas import to_numeric, to_datetime, Timestamp
+from numpy import nan_to_num
 
 DataT = TypeVar('DataT')
 
@@ -11,6 +12,13 @@ class DefaultResponseModel(GenericModel, Generic[DataT]):
     error: Optional[str] = None
     data: DataT
 
+split_by_n1 = lambda x: [i.strip().lower() for i in str(x).split('|')]
+split_by_n2 = lambda x: [i.strip().lower() for i in str(x).split(';')]
+
+placeholder_if_not_instanceof = lambda x, t, p: x if isinstance(x, t) else p
+to_str = lambda x: str(x).strip().lower()
+to_num = lambda x: nan_to_num(to_numeric(x, errors='coerce'))
+to_dt = lambda x: placeholder_if_not_instanceof(to_datetime(x, errors='coerce'), Timestamp, datetime(1970, 1, 1))
 
 class CommonEntityModel(BaseModel):
     inn: str
--- backend/api/utils.py
+++ backend/api/utils.py
@@ -2,13 +2,7 @@
 from logging import error
 from typing import Union
 
-from starlette import status
 from api.model import DefaultResponseModel
-
-from pandas import to_numeric, to_datetime, Timestamp
-from numpy import nan_to_num
-
-from datetime import datetime
 
 
 def success_response(msg: dict = {}) -> DefaultResponseModel[dict]:
@@ -19,10 +13,4 @@ def error_response(msg: Union[str, None] = None) -> DefaultResponseModel[dict]:
     resp = DefaultResponseModel[dict](data={}, error=msg, status=False)
     return resp
 
-split_by_n1 = lambda x: [i.strip().lower() for i in str(x).split('|')]
-split_by_n2 = lambda x: [i.strip().lower() for i in str(x).split(';')]
 
-placeholder_if_not_instanceof = lambda x, t, p: x if isinstance(x, t) else p
-to_str = lambda x: str(x).strip().lower()
-to_num = lambda x: nan_to_num(to_numeric(x, errors='coerce'))
-to_dt = lambda x: placeholder_if_not_instanceof(to_datetime(x, errors='coerce'), Timestamp, datetime(1970, 1, 1))
